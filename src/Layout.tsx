b426cd3ef851439f54cd0f48ecadb43478440c67
kimmiso <owl6068@naver.com>
2023-12-13 16:34:01 +0900

    layout fiexd 확인중

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,11 +6,11 @@ import { windowHeightAtom } from "./utils/atom/commonAtom";
 
 function Layout() {
   const wheight = useRecoilValue(windowHeightAtom);
-  document.body.style.position = "fixed";
-  document.body.style.overflowY = "scroll";
-  document.body.style.width = "100%";
+  // document.body.style.position = "fixed";
+  document.body.style.overflowY = "hidden";
+  // document.body.style.width = "100%";
   setTimeout(() => {
-    document.body.style.position = "static";
+    // document.body.style.position = "static";
     document.body.style.overflow = "unset";
   }, 4500);
 
